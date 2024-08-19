dbb4dda2e164878e77d653a67dabe3652dc340fb
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2024-08-19 13:08:47 +0200

    debug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,22 +29,22 @@ const darkTheme = createTheme({
 export default function Navbar() {
 
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    // const [anchorElUser, setAnchorElUser] = React.useState(null);
   
     const handleOpenNavMenu = (event) => {
       setAnchorElNav(event.currentTarget);
     };
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
+    // const handleOpenUserMenu = (event) => {
+    //   setAnchorElUser(event.currentTarget);
+    // };
   
     const handleCloseNavMenu = () => {
       setAnchorElNav(null);
     };
   
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    }
+    // const handleCloseUserMenu = () => {
+    //   setAnchorElUser(null);
+    // }
 
 
   return  <React.Fragment>
